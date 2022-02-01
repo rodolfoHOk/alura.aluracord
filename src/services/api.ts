178d19932f6eb57af4fcadc70f5373131eceb6ca
rodolfoHOk <rodolfo.okino@gmail.com>
2022-02-01 01:55:02 -0300

    fix api service production url

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiUrl =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:3000/api'
-    : 'https://alura-aluracord-rodolfohok.vercel.app/';
+    : 'https://alura-aluracord-rodolfohok.vercel.app/api';
 
 export const api = axios.create({
   baseURL: apiUrl,
